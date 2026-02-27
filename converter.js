7e1527f5f8473a5a7c9141db7d19c160ebf14a13
Yidnekachew-SK <yidnekachewsk@gmail.com>
2026-02-27 20:55:17 +0300

    Implement decimal to binary conversion
    Added decimal to binary conversion to the existing binary to decimal conversion so users can select from the two options

--- converter.js
+++ converter.js
@@ -1,5 +1,5 @@
 function isBinary (input) {
-	if (Number.isNaN(parseInt(input)) && input.trim() === "") { return false };
+	if (Number.isNaN(parseInt(input)) || input.trim() === "") { return false };
 
 	for(let i = 0; i < input.length; i++) {
 		if (input[i] !== "0" && input[i] !== "1") { 
@@ -16,6 +16,9 @@ function binaryConverter (binaryDigit) {
 	let remainder;
 	let base = 1;
 
+	binaryDigit = parseInt(binaryDigit);
+	if (binaryDigit === 0) { return 0 };
+
 	while(binaryDigit > 0) {
 		remainder = binaryDigit % 10;
 		decimalNumber += remainder * base;
@@ -26,20 +29,73 @@ function binaryConverter (binaryDigit) {
 	return decimalNumber;
 }
 
-function resultDisplay (binary) {
+function decimalConverter (decimalNumber) {
+	let binaryDigit = [];
+	let decimal = parseFloat(decimalNumber);
+	
+	if (Number.isNaN(decimal) || decimalNumber.trim() === "" || !Number.isInteger(decimal)) {
+		return false; 
+	} else if (decimal < 0) {
+		return false;
+	} else if (decimal === 0) {
+		return 0; 
+	}
+
+	while (decimal > 0) {
+		binaryDigit.push(decimal % 2);
+		decimal = Math.floor(decimal / 2);
+	}
+
+	return parseInt(binaryDigit.reverse().join(''));
+}
+
+function binaryConversionDisplay (binary, displayer) {
 	if (binary === "") { 
-		display.textContent = "";
+		displayer.textContent = "";
 	} else if (isBinary(binary)) {
-		display.textContent = binaryConverter(binary);
+		displayer.textContent = binaryConverter(binary);
 	} else {
-		display.textContent = "invalid input";
+		displayer.textContent = "invalid input";
 	}	
 }
 
+function decimalConversionDisplay (decimal, displayer) {
+	let display = document.querySelector(".result");
+	if (decimal === "") { 
+		displayer.textContent = "";
+	} else if (decimalConverter(decimal) !== false) {
+		displayer.textContent = decimalConverter(decimal);
+	} else {
+		displayer.textContent = "invalid input";
+	}	
+}
+
+const resultHeader = document.querySelector("#number-type-displayer");
+const select = document.querySelector("#number-type-selecter");
 const inputs = document.querySelector("#binary-input");
-let display = document.querySelector(".result");
+let numberType = "binary";
+
+select.addEventListener("change", (event) => {
+	if (event.target.value === "binary") {
+		resultHeader.textContent = "Decimal Number:";
+		inputs.placeholder = "1011";
+	} else {
+		resultHeader.textContent = "Binary Number:";
+		inputs.placeholder = "23";
+		numberType = "decimal";
+	}
+});
+
+const display = document.querySelector(".result");
+
 inputs.addEventListener("input", (event) => {
 	let value = event.target.value;
-	resultDisplay(value)
+	if (numberType === "binary") {
+		binaryConversionDisplay(value, display);
+	} else {
+		decimalConversionDisplay(value, display);
+	}
 });
+
+
 
